28d7f3ecb661480c09bbbcfdeaed14c5c7a83ab2
Bee <amy.bee.vanzyl@gmail.com>
2023-10-12 17:21:33 +0100

    tests

--- __tests__/server/routing/helpers.js
+++ __tests__/server/routing/helpers.js
@@ -1,64 +1,3 @@
-// const { Pool } = require('pg');
-// const fs = require('fs');
-// const bcrypt = require("bcrypt")
-// const { v4: uuidv4 } = require("uuid");
-// // const db = require('../../../server/database/connect')
-
-// const dotenv = require('dotenv')
-// dotenv.config()
-
-
-// const reset = fs.readFileSync(__dirname + '/reset.sql').toString();
-
-// const resetTestDB = () => {
-//   return new Promise(async (resolve, reject) => {
-//     try {
-//       const db = new Pool({
-//         connectionString: process.env.DB_URL
-//       });
-//       let run = await db.query(reset)
-//       try {
-//         const passwords = await db.query("SELECT password FROM users")
-
-//         const salt = await bcrypt.genSalt(parseInt(process.env.BCRYPT_SALT_ROUNDS));
-        
-//         passwords.rows.map(async p => {
-//             console.log(p.password)
-//             const hash = await bcrypt.hash(p.password,salt);
-//             await db.query("UPDATE users SET password = $1 WHERE password = $2;",[hash,p.password]) 
-//         });
-//       } catch (err) {
-//         // reject('Could not hash passwords')
-//         reject(err.message)
-//       }
-//       try{
-//         // const result = await Token.create("3")
-//         // result = await Token.create("5")
-//         // result = await Token.createAdmin("3")
-//         let token = uuidv4();
-//         await db.query("INSERT INTO token (users_id, token) VALUES ($1, $2);",
-//         [3, token]);
-//         console.log(3)
-//         token = uuidv4();
-//         await db.query("INSERT INTO token (users_id, token) VALUES ($1, $2);",
-//         [5, token]);
-//         token = uuidv4();
-//         await db.query("INSERT INTO tokenAdmin (admins_id, token) VALUES ($1, $2);",
-//         [3, token]);
-//       }catch(err){
-//         // reject('Could not make tokens')
-//         reject(err.message)
-//       }
-//       resolve('Test DB reset')
-//     } catch (err) {
-//         // reject('Could not reset TestDB')
-//         reject(err.message)
-//     }
-//   })
-// }
-
-// module.exports = { resetTestDB }
-
 const { Pool } = require('pg');
 const fs = require('fs');
 const bcrypt = require("bcrypt");
@@ -87,13 +26,13 @@ const resetTestDB = async () => {
       await db.query("UPDATE users SET password = $1 WHERE password = $2", [hash, p.password]);
     }))
 
-    // Insert tokens
-    const token = uuidv4();
-    await db.query("INSERT INTO token (users_id, token) VALUES ($1, $2)", [3, token]);
-    const token2 = uuidv4();
-    await db.query("INSERT INTO token (users_id, token) VALUES ($1, $2)", [5, token2]);
-    const token3 = uuidv4();
-    await db.query("INSERT INTO tokenAdmin (admins_id, token) VALUES ($1, $2)", [3, token3]);
+    // // Insert tokens
+    // const token = uuidv4();
+    // await db.query("INSERT INTO token (users_id, token) VALUES ($1, $2)", [3, token]);
+    // const token2 = uuidv4();
+    // await db.query("INSERT INTO token (users_id, token) VALUES ($1, $2)", [5, token2]);
+    // const token3 = uuidv4();
+    // await db.query("INSERT INTO tokenAdmin (admins_id, token) VALUES ($1, $2)", [3, token3]);
 
     // Close the database connection
     await db.end();
